13416e6d320057df9f4e2f4ff4149fad2b4e9c28
Ian <ian.gh@the4cranes.co.uk>
2016-03-23 19:42:10 +0000

    Separate DB and view

--- db_gala_routines.php
+++ db_gala_routines.php
@@ -6,88 +6,54 @@ $db_gala_name = "ndsccouk_swim_gala";
 // the Gala name
 function load_gala ($gala_index)
 {
-        global $db_gala_name;
-    	$mysqli = openDatabase($db_gala_name);
+    global $db_gala_name;
+    $mysqli = openDatabase($db_gala_name);
 
-	$stmt = $mysqli->prepare('SELECT `name`, `event_index`, `active`, `event_cost_pence`, UNIX_TIMESTAMP(`closing_date`) FROM `gala_list` WHERE `event_index` = ?');
-	$stmt->bind_param('i', $gala_index);
-	$stmt->execute();
-	$stmt->store_result();
-        
-        // Check to see only one event matches
-	if ($stmt->num_rows != 1) {
-		return "Unable to find a gala for you to enter";
-	}
-        
-	// save it in the session
-       	$stmt->bind_result($name, $event_index, $active, $event_cost_pence, $closing_date);
+    $stmt = $mysqli->prepare('SELECT `name`, `event_index`, `active`, `event_cost_pence`, UNIX_TIMESTAMP(`closing_date`) FROM `gala_list` WHERE `event_index` = ?');
+    $stmt->bind_param('i', $gala_index);
+    $stmt->execute();
+    $stmt->store_result();
 
-	//$row = $res->fetch_assoc();
-	//extract($row);
-        $stmt->fetch();
-	$_SESSION['gala_name'] = $name;
-	$_SESSION['gala_live'] = $active;
-	$_SESSION['event_cost_pence'] = $event_cost_pence;
-	$_SESSION['gala_index'] = $event_index;
-	
-	ob_start();
-	echo "<h2>{$_SESSION['gala_name']}</h2>" ;
-	printf ("<p>Events cost &pound;%.2f each to enter</p>", $event_cost_pence / 100);
-        printf ("<p>Closing date is midnight on %s</p>", date('l jS \of F',$closing_date));
-	$output = ob_get_contents();
-	ob_end_clean();
-	return $output;
+    // Check to see only one event matches
+    if ($stmt->num_rows != 1) {
+        return NULL;
+    }
+
+    // save it in the session
+    $stmt->bind_result($name, $event_index, $active, $event_cost_pence, $closing_date);
+
+    $result = array();
+
+    //$row = $res->fetch_assoc();
+    //extract($row);
+    $stmt->fetch();
+
+    $result['gala_name'] = $name;
+    $result['gala_live'] = $active;
+    $result['event_cost_pence'] = $event_cost_pence;
+    $result['gala_index'] = $event_index;
+    $result['closing_date'] = $closing_date;
+    return $result;
 }
 
-// Gala entry
-// Return HTML to display a table of matching swimmers
- function display_gala_swimmersearch_func ($typedName)
- {
- 	// don't search for very short strings
- 	if (strlen($typedName) < 3) {
- 		return "Search name is too short, please be more specific";
- 	}
- 	//search for names which match the provided name with wild cards before and after
- 	$searchString = '%'.$typedName .'%';
-	global $db_gala_name;
-	$mysqli = openDatabase($db_gala_name);
-	if ($stmt = $mysqli->prepare('SELECT AthleteName, AsaNo FROM athletes WHERE AthleteName LIKE ? ORDER BY AthleteName')) {
-            $stmt->bind_param('s', $searchString);
-            $stmt->execute();
-            $stmt->store_result();
+function searchForSwimmer ($searchString) 
+{
+    global $db_gala_name;
+    $mysqli = openDatabase($db_gala_name);
+    if ($stmt = $mysqli->prepare('SELECT AthleteName, AsaNo FROM athletes WHERE AthleteName LIKE ? ORDER BY AthleteName')) {
+        $stmt->bind_param('s', $searchString);
+        $stmt->execute();
+        $stmt->store_result();
+    }
+    $results = array();
 
-            // Check to see if too many names match
-            if ($stmt->num_rows > 10) {
-                    return "Too many names match your search, please be more specific";
-            }
-            // Check if no names match
-            if ($stmt->num_rows == 0) {
-                    return "No names match your search, please try again";
-            }	
+    // Load the results into an array
+    $stmt->bind_result($athleteName, $asaNo);
 
-            // display the result names
-            $stmt->bind_result($athleteName, $asaNo);
-
-            ob_start();
-            echo 'Select the swimmer you wish enter<br>'. PHP_EOL;
-            echo '<table>'. PHP_EOL; 
-            while ($stmt->fetch()) {
-                    //echo '<tr>';
-                    $argument = "'$athleteName'";
-                    //echo '<td><a onclick="ajaxndscresults_swimmerresults('. $argument .')" >';
-                    echo '<a href="gala_enter?id=', $asaNo, '>', $athleteName, ' - ', $asaNo, '</a>';
-                    //echo "$athleteName</a></td>";
-                    //echo '</tr>'. PHP_EOL;
-            }
-            echo '</table>';	
-
-            $output .= ob_get_contents();
-            ob_end_clean();
-            $mysqli->close();
-            return $output;
-        }
-        else {
-            return 'Error searching for swimmer';
-        }
+    while ($stmt->fetch()) {
+        $results[$asaNo] = $athleteName;
+    }
+    $mysqli->close();
+    
+    return $results;
 }
-
--- gala_entry.php
+++ gala_entry.php
@@ -12,7 +12,22 @@ $db_gala_name = "ndsccouk_swim_gala";
 function gala_entry_start_func ($atts) {
     extract( shortcode_atts( array('gala_index' => '1'), $atts ) );
     //echo "Gala index is $gala_index\n";
-    return load_gala((int)$gala_index);
+    $gala_details = load_gala((int)$gala_index);
+    
+    if ($gala_details == NULL) {
+        return "Unable to find an active Gala to enter";
+    }
+    
+    //TODO Save gala details to session
+    
+    ob_start();
+    echo "<h2>{$gala_details['gala_name']}</h2>" ;
+    printf ("<p>Events cost &pound;%.2f each to enter</p>", $gala_details['event_cost_pence'] / 100);
+    printf ("<p>Closing date is midnight on %s</p>", date('l jS \of F',$gala_details['closing_date']));
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+
 }
 add_shortcode( 'GALAENTRYSTART', 'gala_entry_start_func' );
 
@@ -49,6 +64,39 @@ function ajaxndscgala_swimmersearch_ajaxhandler() {
 	die($results);
 }
 
+
+// Gala entry
+// Return HTML to display a table of matching swimmers
+ function display_gala_swimmersearch_func ($typedName)
+ {
+ 	// don't search for very short strings
+ 	if (strlen($typedName) < 3) {
+ 		return "Search name is too short, please be more specific";
+ 	}
+ 	//search for names which match the provided name with wild cards before and after
+ 	$searchString = '%'.$typedName .'%';
+        $results = searchForSwimmer($searchString);
+
+        if (count($results) < 1) {
+            return "No swimmers matched that search, please try again";
+        }
+        else if (count($results) > 10) {
+            return "Too many swimmers matched that search, please be more specific";
+        } 
+        
+        ob_start();
+        echo 'Select the swimmer you wish enter<br>'. PHP_EOL;
+        foreach ($results as $asaNo => $athleteName) {
+                $argument = "'$athleteName'";
+                echo '<a href="fetch_swimmer?id=', $asaNo, '">', $athleteName, ' - ', $asaNo, '</a></br>'. PHP_EOL;
+        }
+
+        $output .= ob_get_contents();
+        ob_end_clean();
+        return $output;
+}
+
+
  // register swimmer search handler with Wordpress
 add_action( 'wp_ajax_nopriv_ajaxndscgala_swimmersearch_ajaxhandler', 'ajaxndscgala_swimmersearch_ajaxhandler' );
 add_action( 'wp_ajax_ajaxndscgala_swimmersearch_ajaxhandler', 'ajaxndscgala_swimmersearch_ajaxhandler' ); 
@@ -59,3 +107,38 @@ function ajaxndscgala_enqueuescripts() {
 	wp_localize_script( 'ajaxndscgala', 'ajaxndscgalaajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 }
 add_action('wp_enqueue_scripts', ajaxndscgala_enqueuescripts);
+
+
+// retrieve athlete data and store in session
+// display athlete name
+// gets the chosen athlete
+// put on a new page called fetch_swimmer
+function gala_entry_retrieve_athelete_func ($atts) {
+	if (!array_key_exists("swimmer", $_GET)) {
+		$_SESSION['submit_disabled'] = TRUE;
+		return 'Somthing went wrong, No swimmer ID provided';
+	}
+
+	// Get athlete data from database
+	$mysqli = openNDSC();
+	$sql = "SELECT Last, First, ID_NO, Sex, Birth, `Group` FROM `athlete` WHERE `Inactive`=0 AND SubGr='SW' AND (ID_NO='". $_GET["swimmer"] ."') ORDER BY Last ASC";
+	$res = getResults ($mysqli, $sql);
+
+	if ($res->num_rows == 0) {
+		return "Somthing went wrong, couldn't find swimmer: ". $_GET["swimmer"];
+	}
+	if ($res->num_rows > 1) {
+		return "Somthing went wrong, more than one swimmer with that ID: ". $_GET["swimmer"];
+	}
+
+	$row = $res->fetch_assoc();
+	extract($row);
+	// setup the session variables
+	$_SESSION['swimmer_name']= $First .' '. $Last;
+	$_SESSION['swimmer_ID']=$ID_NO;
+	$_SESSION['athlete_group']=$Group;
+	$_SESSION['gender']=$Sex;
+	
+	return "Name: {$_SESSION['swimmer_name']} <br>ID: $ID_NO <br>Squad: $Group <br>". PHP_EOL;
+}
+add_shortcode( 'gala_entry_retrieve_athelete', 'gala_entry_retrieve_athelete_func' );
