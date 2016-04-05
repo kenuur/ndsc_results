a570d22d40802705deeb79e0db66e58a295f873a
Ian <ian.gh@the4cranes.co.uk>
2016-04-05 17:25:17 +0100

    Confirm entry page

--- gala_entry.php
+++ gala_entry.php
@@ -137,8 +137,8 @@ function gala_entry_retrieve_athelete ($atts) {
     if (!array_key_exists("id", $_GET)) {
             return 'Somthing went wrong, No swimmer ID was provided';
     }
-    $swimmerID = $_GET["id"];
-
+    //$swimmerID = $_GET["id"];
+    $swimmerID = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING);
     // Get athlete data from database
     $swimmerDetails = fetchSwimmer($swimmerID);
 
@@ -163,40 +163,42 @@ function gala_entry_displayevents() {
 	// get events from the dataabse that match the gala and gender of swimmer
     $events = fetchEvents($_SESSION['gala_index'], $_SESSION['gender']);
 	
-	// display a form that will allow user to select events and enter time
-	ob_start();
-        echo 'Enter a time for events you wish to enter like 1:23.45 or 57.89<br>';
-        echo 'If you dont have a time enter NT<br>';
-        echo 'Leave the time blank to not enter that event<br><br>' . PHP_EOL;
-	echo '<form action="gala-entry-3"><table>';
-        echo '<tr><th>Event Number</th>';
-        echo '<th>Event Name</th>';
-        echo '<th>Entered time</th></tr>';
-        $event_names = array();
-	foreach ($events as $event) {
-		echo '<tr>';
-		echo '<td>' . $event['number'] . '<input type="hidden" name="eventnos[]" value="' . $event['number'] . '"></td>' . PHP_EOL;
-		echo '<td>' . $event['name'] . '</td>' . PHP_EOL;
-                $event_names[] = $event['name'];
-		echo '<td><input type="text" name="entered_times[]" maxlength="8" size="8" maxlength="8" ';
-		echo 'pattern="^([1-2]?[0-9]:)?[0-5][0-9]\.[0-9]{2}$|NT|nt" title="time like 1:23.45 or 57.89 or NT or blank if not entering"';
-		echo '></td></tr>'. PHP_EOL;
-        }
-	echo '</table><input type="submit" value="Submit"></form>'; 
-        $_SESSION['eventnames'] = $event_names;
-	$output = ob_get_contents();
-	ob_end_clean();
-	return $output;		
+    // display a form that will allow user to select events and enter time
+    ob_start();
+    echo 'Enter a time for events you wish to enter like 1:23.45 or 57.89<br>';
+    echo 'If you dont have a time enter NT<br>';
+    echo 'Leave the time blank to not enter that event<br><br>' . PHP_EOL;
+    echo '<form action="\gala-entry-3" method="post"><table>';
+    echo '<tr><th>Event Number</th>';
+    echo '<th>Event Name</th>';
+    echo '<th>Entered time</th></tr>';
+    $event_names = array();
+    foreach ($events as $event) {
+            echo '<tr>';
+            echo '<td>' . $event['number'] . '<input type="hidden" name="eventnos[]" value="' . $event['number'] . '"></td>' . PHP_EOL;
+            echo '<td>' . $event['name'] . '</td>' . PHP_EOL;
+            $event_names[] = $event['name'];
+            echo '<td><input type="text" name="entered_times[]" maxlength="8" size="8" maxlength="8" ';
+            //echo 'pattern="^([1-2]?[0-9]:)?[0-5][0-9]\.[0-9]{2}$|NT|nt" title="time like 1:23.45 or 57.89 or NT or blank if not entering"';
+            echo '></td></tr>'. PHP_EOL;
+    }
+    echo '</table><input type="submit" value="Submit"></form>'; 
+    $_SESSION['eventnames'] = $event_names;
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;		
 }
 
-
+// display the entered events with the entry time
+// if any times are formated badly prevent confirmation
+// 
 function gala_entry_eventsconfirm_func($atts) {
     $entryDetails = array();
     
     // check all the arrays are populated
-    $entryDetails['times'] = cleanEntryTimes($_GET['entered_times']) ;
+    $entryDetails['times'] = cleanEntryTimes($_POST['entered_times']) ;
     $eventCount = count($entryDetails['times']);
-    $entryDetails['eventno'] = $_GET['eventnos'];
+    $entryDetails['eventno'] = $_POST['eventnos'];
     if ($eventCount != count ($entryDetails['eventno'])) { return 'System error, please try again'; }
     $entryDetails['eventname'] = $_SESSION['eventnames'];
     if ($eventCount != count ($entryDetails['eventname'])) { return 'System error, please try again'; }
@@ -204,19 +206,56 @@ function gala_entry_eventsconfirm_func($atts) {
     
 
     $indx = 0;
+    $entryCount = 0;
+    $submitDisabled = "";
     ob_start();
+    echo 'Summary of entry:';
     echo '<table>';
     while ($indx < $eventCount) {
-        echo '<tr>';
-        echo '<td>' . $entryDetails['eventno'][$indx] . '</td>';
-        echo '<td>' . $entryDetails['eventname'][$indx] . '</td>';
-        echo '<td>' . $entryDetails['times'][$indx] . '</td>';
-        echo '</tr>';
+        // only display enetered events
+        if ($entryDetails['times'][$indx] !=  "NOENTRY" ) {
+            $entryCount++;
+            echo '<tr>';
+            echo '<td>' . $entryDetails['eventno'][$indx] . '</td>';
+            echo '<td>' . $entryDetails['eventname'][$indx] . '</td>';
+            echo '<td>' . $entryDetails['times'][$indx] . '</td>';
+            echo '</tr>';
+            if ($entryDetails['times'][$indx] ==  "Invalid" ) {
+                $submitDisabled = "Disabled";
+            }
+        }
         $indx++;
     }
     echo '</table>';
+    if ($entryCount > 0) {
+
+        if ($submitDisabled == "Disabled") {
+            echo 'You have entered one or more times in an invalid format<br>';
+            echo 'Please use seconds and two decimals e.g. 12.34 ';
+            echo 'or minutes, seconds and decimals e.g. 1:23.45<br>';
+            echo 'Please go back and correct your entry<p>';
+        }
+        else {
+            printf ('You have entered %d events<br>', $entryCount);
+            echo 'The cost of entry is: ' . entryCost($entryCount) . '<br>';
+            echo 'If you are happy with the entry click the confirm button below. ';
+            echo 'Otherwise go back to make any corrections<br>';
+        }
+    }
+    else {
+        echo 'You have not entered any events, please go back and enter a time (or NT) for one or more events<p>';
+        $submitDisabled = "Disabled";
+    }
+    echo '<p>';
+    echo '<form action="gala-entry-4">';
+    //printf('<input type="image" name=submit value="GoCardless" src="https://s3-eu-west-1.amazonaws.com/gocardless/images/public/buttons/updated/pay-with-gc-small.png" alt="Submit" %s><br>', $submitDisabled);
+    printf('<input type="submit" value="Confirm" %s></form>', $submitDisabled);
     $output = ob_get_contents();
     ob_end_clean();
+    // store the entry details in the session if valid
+    if ($submitDisabled != "Disabled") {
+        $_SESSION['entryDetails'] = $entryDetails;
+    }
     return $output;	
 }
 add_shortcode( 'gala_entry_eventsconfirm', 'gala_entry_eventsconfirm_func' );
@@ -254,3 +293,19 @@ function cleanEntryTimes ($entryTimes)
     }
     return $clean_entrytimes;
 }
+
+
+// calculate and return the cost for the number of entries
+// store the cost in the session
+function entryCost($numEvents) {
+    $entryCost = ($numEvents * $_SESSION['event_cost_pence']) / 100;
+    $_SESSION['total_entry_cost'] = $entryCost;
+    return sprintf('£%.2f', $entryCost);
+}
+
+// save the entry in the database
+// display the payment options
+function gala_entry_save_entry_func() {
+    
+}
+add_shortcode( 'gala_entry_save_entry', 'gala_entry_save_entry_func' );
